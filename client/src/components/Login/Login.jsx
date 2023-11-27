f969caf6f705a3929f77e5b9f51774834b41c5dd
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-11-27 12:54:12 -0700

    Updated login file, needs tweaking, being irrational and lame

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+//import React from 'react';
 import spoderLogo from '../../assets/images/SPODER.png';
 import './style.css';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutation';
+
+import Auth from '../../utils/auth'
 
 const Login = ({ onToggleView }) => {
+
+	const [loginForm, setLoginData] = useState({ username: '', password: '' });
+	const [login, { error, data }] = useMutation(LOGIN_USER);
+	// update state based on form input changes
+	const handleChange = (event) => {
+	const { name, value } = event.target;
+	setLoginData({
+		...loginForm,
+		[name]: value,
+	});
+	};
+	// submit form
+	const handleFormSubmit = async (event) => {
+	event.preventDefault();
+	console.log(loginForm);
+	try {
+		const { data } = await login({
+		variables: { ...loginForm },
+		});
+		Auth.login(data.login.token);
+	} catch (e) {
+		console.error(e);
+	}
+	// clear form values
+	setLoginData({
+		username: '',
+		password: '',
+	});
+	};
 	return (
 		<div className='login min-h-screen flex items-center justify-center'>
 			<div className='bg-white bg-opacity-20 px-8 shadow-md rounded-md w-96'>
 				<img src={spoderLogo} alt='spider logo' width='278' height='180'></img>
 				<h2 className='text-2xl font-bold mb-3'>Login</h2>
-				<form>
+				<form onSubmit={handleFormSubmit}>
 					<div className='mb-4'>
 						<label
 							htmlFor='username'
@@ -20,7 +54,8 @@ const Login = ({ onToggleView }) => {
 							type='text'
 							id='username'
 							name='username'
-							className='w-full p-2 border rounded-md focus:outline-none focus:border-blue-500'
+							className='w-full p-2 border rounded-md focus:outline-none focus:border-blue-500' value={loginForm.username}
+							onChange={handleChange}
 						/>
 					</div>
 					<div className='mb-4'>
@@ -34,7 +69,8 @@ const Login = ({ onToggleView }) => {
 							type='password'
 							id='password'
 							name='password'
-							className='w-full p-2 border rounded-md focus:outline-none focus:border-blue-500'
+							className='w-full p-2 border rounded-md focus:outline-none focus:border-blue-500' value={loginForm.password}
+							onChange={handleChange}
 						/>
 					</div>
 					<button
@@ -44,10 +80,13 @@ const Login = ({ onToggleView }) => {
 						Login
 					</button>
 					<div className='register'>
-						<p>Don't Have an Account?</p>
+						<p>Dont Have an Account?</p>
+						{/*<p>
+							<Link to="/landingpage">Register Here</Link>
+						</p>*/}
 						<button onClick={onToggleView} className='text-purple-300 hover:text-purple-200'>
 							Register Here
-						</button>
+	</button>
 					</div>
 				</form>
 			</div>
@@ -56,62 +95,3 @@ const Login = ({ onToggleView }) => {
 };
 
 export default Login;
-
-// START OF ORIGINAL CODE
-// import { React } from 'react';
-// import spoderLogo from '../../assets/images/SPODER.png';
-// import './style.css'
-
-// //pending login for usemutation and signup
-// const Login = () => {
-// 	return (
-// 		<div className='login min-h-screen flex items-center justify-center'>
-// 			<div className='bg-white bg-opacity-20 px-8 shadow-md rounded-md w-96'>
-// 				<img src={spoderLogo} alt='spider logo' width='278' height='180'></img>
-// 				<h2 className='text-2xl font-bold mb-3'>Login</h2>
-// 				<form>
-// 					<div className='mb-4'>
-// 						<label
-// 							htmlFor='username'
-// 							className='block text-gray-300 text-sm font-medium mb-2'
-// 						>
-// 							Username
-// 						</label>
-// 						<input
-// 							type='text'
-// 							id='username'
-// 							name='username'
-// 							className='w-full p-2 border rounded-md focus:outline-none focus:border-blue-500'
-// 						/>
-// 					</div>
-// 					<div className='mb-4'>
-// 						<label
-// 							htmlFor='password'
-// 							className='block text-gray-300 text-sm font-medium mb-2'
-// 						>
-// 							Password
-// 						</label>
-// 						<input
-// 							type='password'
-// 							id='password'
-// 							name='password'
-// 							className='w-full p-2 border rounded-md focus:outline-none focus:border-blue-500'
-// 						/>
-// 					</div>
-// 					<button
-// 						type='submit'
-// 						className='w-full bg-purple-950 text-white p-1 rounded-md hover:bg-purple-400 focus:outline-none focus:shadow-outline-blue'
-// 					>
-// 						Login
-// 					</button>
-// 					<div className='register'>
-// 						<p>Don't Have an Account?</p>
-// 						<a href='' className='text-purple-300 hover:text-purple-200'>Register Here</a>
-// 					</div>
-// 				</form>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Login;
