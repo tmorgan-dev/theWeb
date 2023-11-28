9a8e8688f027b49dbde801220bb4ebc269bd2de0
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-27 21:07:02 -0700

    added logout link to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,16 +2,44 @@ import { Link } from 'react-router-dom';
 import React from "react";
 import Navigation from '../Navigation/Navigation';
 import spoderLogo from '../../assets/images/SPODER.png';
-import './style.css'
+import './style.css';
+import AuthService from '../../utils/auth';
 
 function Header() {
+	const isLoggedIn = AuthService.loggedIn();
+
 	return (
 		<nav className='text-white fixed w-full top-O'>
-            <div className='container flex items-center'>
-				<img src={spoderLogo} alt='spider logo' className='mt-1 mr-2' width='50' height='50'></img>
-				<div className='logo-navbar-brand'>theWeb</div>
+			<div className='container flex items-center justify-between'>
+				<div className='flex items-center'>
+					<img src={spoderLogo} alt='spider logo' className='mt-1 mr-2' width='50' height='50'></img>
+					<div className='logo-navbar-brand'>theWeb</div>
+				</div>
+				{isLoggedIn && (
+					<Link to="#" className="logout-link" onClick={() => AuthService.logout()}>Logout</Link>
+				)}
 			</div>
 		</nav>
 	);
 }
+
 export default Header;
+
+// START OF ORIGINAL CODE
+// import { Link } from 'react-router-dom';
+// import React from "react";
+// import Navigation from '../Navigation/Navigation';
+// import spoderLogo from '../../assets/images/SPODER.png';
+// import './style.css'
+
+// function Header() {
+// 	return (
+// 		<nav className='text-white fixed w-full top-O'>
+//             <div className='container flex items-center'>
+// 				<img src={spoderLogo} alt='spider logo' className='mt-1 mr-2' width='50' height='50'></img>
+// 				<div className='logo-navbar-brand'>theWeb</div>
+// 			</div>
+// 		</nav>
+// 	);
+// }
+// export default Header;
