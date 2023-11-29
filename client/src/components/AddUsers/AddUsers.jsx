52b46354d84203c7a10de8009920359a6de4809a
saevans86 <sam_evans1986@hotmail.com>
2023-11-28 18:39:31 -0700

    updating adduser file

---
+++ client/src/components/AddUsers/AddUsers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useMutation } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { ADD_USER } from '../../utils/mutation'
+import { QUERY_ME } from "../../utils/queries";
+
+const AddUser = ({username}) => {
+    return (
+
+        <div>
+
+        </div>
+    )
+}
+    
+    
+
+export default AddUser
