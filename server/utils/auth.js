93897f2aebdad1a476fe346f423c90626c46df56
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-30 15:24:49 -0700

    added logic to postlist.jsx, auth.js, querries.js, and resolves.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -18,24 +18,24 @@ module.exports = {
 			req.body.token ||
 			req.query.token ||
 			req.headers.authorization;
-
+	
 		if (req.headers.authorization) {
 			token = token.split(' ').pop().trim();
 		}
-
+	
 		if (!token) {
 			return req;
 		}
-
+	
 		try {
 			const { data } = jwt.verify(token, secret, {
 				maxAge: expiration,
 			});
 			req.user = data;
-		} catch {
-			console.log('Invalid token');
+		} catch (error) {
+			console.error('Error verifying token:', error.message);
 		}
-
+	
 		return req;
 	},
 	signToken: function ({ email, username, _id }) {
