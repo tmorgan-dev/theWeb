e7c708d35a567de1345c9b799a4d800980cfaf37
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-11-29 19:15:25 -0700

    Added post/comment functionality, still needs work in the react files though

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -24,18 +24,18 @@ export const QUERY_USER = gql`
   }
 `;
 
-// export const QUERY_POSTS = gql`
-//   query getPosts {
-//     thoughts {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//     }
-//   }
-// `;
+export const QUERY_POSTS = gql`
+  query getPosts {
+    post {
+      _id
+      postText
+      postAuthor
+      createdAt
+    }
+  }
+`;
 
-export const QUERY_COMMENTS = gql`
+export const QUERY_SINGLE_POST = gql`
   query getSinglePost($postId: ID!) {
     post(postId: $postId) {
       _id
